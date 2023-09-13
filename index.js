f6c6c9af118fcf0b76edb933caffe4e066d853bf
Chun Lau <dlaucodes@gmail.com>
2023-09-13 17:36:24 -0400

    fix spacing

---
+++ index.js
@@ -0,0 +1,9 @@
+// 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
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-heoVVmXNssFcmS0qtxCLkwTa",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
