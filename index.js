fa05410350de178557a082e71e476eef033a5167
Chun Lau <dlaucodes@gmail.com>
2023-09-13 17:55:20 -0400

    app key

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
     organization: "org-heoVVmXNssFcmS0qtxCLkwTa",
-    apiKey: "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",
+    apiKey: `${process.env.CHAT_APP_KEY}`,
 });
 const openai = new OpenAIApi(configuration);
 // const response = await openai.listEngines();
